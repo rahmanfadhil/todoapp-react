7df63ceb7b15f46ca0565eb38d77aad3c1987c8e
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-04-23 13:56:25 +0700

    Add feature create read delete

---
+++ src/small/output.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Output = (props) => (
+  <div>
+    <ul>
+      {props.todos.map((item, index) => (
+        <li key={index}>
+          <button onClick={() => props.deleteTodo(index)}>X</button>
+          {item.todo}
+        </li>
+      ))}
+    </ul>
+  </div>
+)
+
+export default Output;
